557ac3eb6a3bc10b8d5cd695c2c2252ad30a68b4
AdrienV17 <45920897+AdrienV17@users.noreply.github.com>
2020-04-16 23:47:54 -0500

    Finishing Front-End

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
 import Clarifai from 'clarifai';
-import { Navigation } from './components/Navigation/Navigation';
+import Navigation from './components/Navigation/Navigation.jsx';
+import SignIn from './components/SignIn/SignIn';
+import Register  from './components/Register/Register';
 import Logo  from './components/Logo/Logo';
 import Rank  from './components/Rank/Rank';
 import ImageLinkForm  from './components/ImageLinkForm/ImageLinkForm';
 import FaceRecognition  from './components/FaceRecognition/FaceRecognition';
-
 import './App.css';
+
 
 
 const app = new Clarifai.App({
@@ -33,42 +35,75 @@ class App extends Component {
     super()
     this.state = {
       input: '',
-      imageUrl: ''
-
+      imageUrl: '',
+      box: {},
+      route: 'SignIn',
+      isSignedIn: false
+      
     }
   }
 
+  calculateFaceLocation = (data) =>{
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol:clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol:width -(clarifaiFace.right_col * width),
+      bottomRow: height -(clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) =>{
+    this.setState({box: box})
+  }
   onInputChange = (event) => {
     this.setState({input: event.target.value})
   } 
+
+  onRouteChange = (route) => {
+    if(route === 'SignOut'){
+      this.setState({isSignedIn:false})
+    }else if(route === 'home'){
+      this.setState({isSignedIn:true})
+    }
+    this.setState({route:route})
+  }
+
+   
 
   onSubmit = () => {
     this.setState({imageUrl: this.state.input})
     app.models.predict(
       Clarifai.FACE_DETECT_MODEL,
-      this.state.input).then(
-      function(response) {
-        console.log(response.outputs[0].data.regions[0].region_info.bounding_box)
-      },
-      function(err) {
-        // there was an error
-      }
-    ); 
+      this.state.input).then(response=> this.displayFaceBox(this.calculateFaceLocation(response))) 
+      .catch(err=>console.log(err))
+       
   }
   render(){
-
+    const { isSignedIn, imageUrl, route, box } = this.state;
     return (
       <div className="App">
            <Particles 
               className='particles'
                 params={particlesOptions}
-              />
-          <Navigation />
-          <Logo />
-          <Rank />
-          <ImageLinkForm onInputChange={this.onInputChange}  onSubmit={this.onSubmit}/>
-          <FaceRecognition imageUrl={this.state.imageUrl}/> 
-  
+                />
+          <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/> 
+          {route === 'home'
+            ?<div>
+                <Logo />
+                <Rank />
+                <ImageLinkForm onInputChange={this.onInputChange}  onSubmit={this.onSubmit}/>
+                <FaceRecognition box={box}  imageUrl={imageUrl}/> </div>
+            : ( route === 'SignIn'
+              ?<SignIn onRouteChange={this.onRouteChange}/>
+              :<Register onRouteChange={this.onRouteChange}/>
+
+            )
+          }
+        
       </div>
     );
   }
--- src/components/FaceRecognition/FaceRecognition.jsx
+++ src/components/FaceRecognition/FaceRecognition.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import './FaceRecognition.css';
 
 
 
-
-const FaceRecognition = ({ imageUrl }) => {
+const FaceRecognition = ({ imageUrl,box }) => {
     return(
         <div className='center'>
             <div className="absolute mt2">
+                <img id='inputimage' src={imageUrl} alt="" width='500px' height='auto'/>
+                <div className="bounding-box" style={{top: box.topRow, right:box.rightCol,bottom:box.bottomRow,left:box.leftCol}}>
 
-            <img src={imageUrl} alt="" width='500px' height='auto'/>
+                </div>
             </div>
         </div>
     );
